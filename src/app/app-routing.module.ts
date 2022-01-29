9dcbf423b1a158569a2eb44be99aedad6802faa8
afilho_ <dinho-16@hotmail.com>
2022-01-28 21:07:05 -0300

    para meu uso e para praticar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './views/form/form.component';
+import { IndexComponent } from './views/index/index.component';
+import { ListComponent } from './views/list/list.component';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+
+    //component: LoginComponent
+    component: IndexComponent
+    //component: FormComponent
+    //component: ListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './views/form/form.component';
+import { IndexComponent } from './views/index/index.component';
+import { ListComponent } from './views/list/list.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    IndexComponent,
+    ListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
